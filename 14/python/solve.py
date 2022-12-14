0984e7a4a78a33a554853dcca60b13633454961f
Martin D. Jaere <mdjaere@gmail.com>
2022-12-14 22:26:25 +0000

    Clean up Day 14@

--- 14/python/solve.py
+++ 14/python/solve.py
@@ -4,7 +4,7 @@ infile = sys.argv[1] if len(sys.argv) > 1 else 'input'
 lines = [line.strip() for line in open(infile)]
 
 rocks = set()
-floor = 0
+max_y = 0
 for line in lines:
     coords = [[int(x) for x in f.split(",")] for f in line.split(" -> ")]
     for i in range(len(coords) - 1):
@@ -14,36 +14,33 @@ for line in lines:
         ys = sorted([s[1], e[1]])
         for x in range(xs[0], xs[1] + 1):
             for y in range(ys[0], ys[1] + 1):
-                floor = max(floor, y+2)
+                max_y = max(max_y, y)
                 rocks.add((x, y))
 
 
-def settle_grain(grain, solids, include_floor=False):
+def settle_grain(grain, solids):
     gr_x, gr_y = grain
-    if include_floor and gr_y == floor - 1:
-        return False, grain
+    if gr_y == max_y + 1:
+        return grain
     candidates = [
         (gr_x, gr_y + 1),
         (gr_x - 1, gr_y + 1),
         (gr_x + 1, gr_y + 1)
     ]
     for c in candidates:
-        if c[1] > floor:
-            return True, None
         if c not in solids:
-            return settle_grain(c, solids, include_floor)
-    if grain == (500, 0):
-        return True, grain
-    return False, grain
+            return settle_grain(c, solids)
+    # This grain cannot go anywhere
+    return grain
 
 
 # Part 1
 solids_1 = set(rocks)
-solved = False
-while not solved:
-    solved, grain = settle_grain((500, 0), solids_1, include_floor=False)
-    if grain:
-        solids_1.add(grain)
+while True:
+    grain = settle_grain((500, 0), solids_1)
+    if grain[1] > max_y:
+        break
+    solids_1.add(grain)
 
 ans_1 = len(solids_1.difference(rocks))
 assert(ans_1 == 901)
@@ -52,12 +49,12 @@ print(ans_1)
 
 # Part 2
 solids_2 = set(rocks)
-solved = False
-while not solved:
-    solved, grain = settle_grain((500, 0), solids_2, include_floor=True)
-    if grain:
-        solids_2.add(grain)
-
+while True:
+    grain = settle_grain((500, 0), solids_2)
+    solids_2.add(grain)
+    if grain == (500, 0):
+        break
+        
 ans_2 = len(solids_2.difference(rocks))
 assert(ans_2 == 24589)
 print(ans_2)
